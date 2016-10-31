e4a9d5cd5b66dafac8e62432142be265749d6692
spockuto <venkythesupersaiyan@gmail.com>
2016-10-31 22:14:16 +0530

    Initial Commit

---
+++ blake2s.c
@@ -0,0 +1,67 @@
+/* 
+	A simple Blake2s implementation.
+
+	Authors : Venkkatesh Sekar , Suhith Rajesh
+
+	Standards : RFC 7693 / Nov 2015
+
+				  | BLAKE2b          | BLAKE2s          |
+    --------------+------------------+------------------+
+   	|Bits in word | w = 64           | w = 32           |
+    |Rounds in F  | r = 12           | r = 10           |
+   	|Block bytes  | bb = 128         | bb = 64          |
+    |Hash bytes   | 1 <= nn <= 64    | 1 <= nn <= 32    |
+    |Key bytes    | 0 <= kk <= 64    | 0 <= kk <= 32    |
+    |Input bytes  | 0 <= ll < 2**128 | 0 <= ll < 2**64  |
+    --------------+------------------+------------------+
+   	|G Rotation   | (R1, R2, R3, R4) | (R1, R2, R3, R4) |
+    | constants = | (32, 24, 16, 63) | (16, 12,  8,  7) |
+   	--------------+------------------+------------------+
+
+   	These variables are used in the algorithm description:
+
+      IV[0..7]  Initialization Vector (constant).
+
+   SIGMA[0..9]  Message word permutations (constant).
+
+       p[0..7]  Parameter block (defines hash and key sizes).
+
+      m[0..15]  Sixteen words of a single message block.
+
+       h[0..7]  Internal state of the hash.
+
+    d[0..dd-1]  Padded input blocks.  Each has "bb" bytes.
+
+             t  Message byte offset at the end of the current block.
+
+             f  Flag indicating the last block.
+
+    Using official blake2.h , blake2-impl.h
+*/
+
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "blake2.h"
+#include "blake2-impl.h"
+
+static const uint32_t blake2s_IV[8] =
+{
+  0x6A09E667UL, 0xBB67AE85UL, 0x3C6EF372UL, 0xA54FF53AUL,
+  0x510E527FUL, 0x9B05688CUL, 0x1F83D9ABUL, 0x5BE0CD19UL
+};
+
+static const uint8_t blake2s_SIGMA[10][16] =
+{
+  {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
+  { 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 } ,
+  { 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 } ,
+  {  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 } ,
+  {  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 } ,
+  {  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 } ,
+  { 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 } ,
+  { 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 } ,
+  {  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 } ,
+  { 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 } ,
+};
