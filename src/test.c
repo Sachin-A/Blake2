1021073c9b9e628836e58d2fd1ee03ff67a42377
Soorya Narayan <nayaran.ayroos@gmail.com>
2016-12-22 13:23:06 +0530

    Fix input assignment to pass numeric tests

--- src/test.c
+++ src/test.c
@@ -5,35 +5,47 @@
 #define BUF_LENGTH 256
 
 void
-print_hex(const uint8_t* hash)
+print_hex(const uint8_t* hash, char* string, int len)
 {
   size_t i;
 
-  i=0;
-  printf("hash  :");
-  while (i++ < BLAKE2B_OUTBYTES) {
+  i = 0;
+  printf("%s: ", string);
+  while (i++ < len) {
     printf("%02x ", *hash++);
   }
+  printf("\n");
 }
 
 int
 main(int argc, char const* argv[])
 {
 
-  char buf[] = "abc";
-  char key[] = "";
+  uint8_t key[BLAKE2B_KEYBYTES];
+  uint8_t buf[BUF_LENGTH];
   uint8_t hash[BLAKE2B_OUTBYTES];
-  size_t buflen = strlen(buf);
-  size_t keylen = strlen(key);
-  size_t i;
+  size_t i, offset;
 
-  for (i = 0; i <= buflen; ++i) {
-    blake2b(hash, BLAKE2B_OUTBYTES, buf, i, key, keylen);
+  // keyed hashing not implemented yet
+  for (i = 0; i < BLAKE2B_KEYBYTES; ++i)
+    key[i] = (uint8_t)i;
+
+  offset = 256;
+
+  for (i = 0; i < offset; ++i)
+    buf[i] = (uint8_t)i;
+
+  for (i = offset; i < BUF_LENGTH; ++i)
+    buf[i] = (uint8_t)0;
+
+  /* Test simple API */
+  for (i = 0; i < offset; ++i) {
+    blake2b(hash, BLAKE2B_OUTBYTES, buf, i, key, BLAKE2B_KEYBYTES);
   }
 
-  printf("input :%s\n", buf);
-  printf("key   :%s\n", key);
-  print_hex(hash);
+  print_hex(buf, "buffer", offset);
+  print_hex(key, "key", 0);
+  print_hex(hash, "hash", BLAKE2B_OUTBYTES);
 
   return 0;
 }
