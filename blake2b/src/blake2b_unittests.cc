94e666fecf18dfcef0f715a10d1ba07421b08852
Sachin <sachinashok.g@gmail.com>
2017-01-11 07:10:22 +0530

    Add SetUp() & TearDown() functions
    Create test fixtures
    Derive from ::testing::Test as public
    SetUp() function to prepare objects for each test
    Teardown() function to release any resources allocated

--- blake2b/src/blake2b_unittests.cc
+++ blake2b/src/blake2b_unittests.cc
@@ -13,6 +13,31 @@ public:
   uint8_t correct[BLAKE2B_OUTBYTES];
 };
 
+class KnownAnswerTests : public ::testing::Test {
+public:
+  virtual void SetUp() {
+    values = new testvalues();
+    size_t i;
+    /* Key of the form (i, i+1 ... i+63 where i=0) */
+    for (i = 0; i < BLAKE2B_KEYBYTES; ++i) {
+      values->key[i] = (uint8_t)i;
+    }
+
+    for (i = 0; i < BLAKE2_KAT_LENGTH; ++i) {
+      values->buf[i] = (uint8_t)0;
+    }
+    /* Buffer of the form (i, i+1 ... i+255 where i=0) */
+    for (i = 0; i < BLAKE2_KAT_LENGTH; ++i) {
+      values->buf[i] = (uint8_t)i;
+    }
+  }
+
+  virtual void TearDown() {
+    delete values;  
+  }
+  testvalues *values;
+};
+
 int main(int argc, char **argv) {
 
   testing::InitGoogleTest(&argc, argv);
